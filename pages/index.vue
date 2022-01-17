48a8a510a5871bf88fa5f6d556c93b106d3ff9c0
Bau Huynh <bauhuynhdev@gmail.com>
2022-01-17 17:01:53 +0700

    Update fetch api

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,44 @@
 <template>
-  <Tutorial/>
+  <div>
+    <ul>
+      <li v-for="item in items">{{ item.id }} - {{ item.title }}</li>
+    </ul>
+    <ul class="pagination">
+      <li v-for="page in pages">
+        <NuxtLink :to="{query: {page}}">{{ page }}</NuxtLink>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  watchQuery: ['page'],
+  async asyncData({$axios, route}) {
+    const page = route.query.page || 1;
+    const perPage = 15;
+    const start = (page - 1) * perPage;
+    const end = page * perPage;
+    const {data} = await $axios.get('https://jsonplaceholder.typicode.com/posts');
+    const total = data.length;
+    const items = data.slice(start, end)
+    const pages = Math.round(total / perPage);
+    return {
+      items,
+      total,
+      pages
+    }
+  }
 }
 </script>
+
+<style>
+.pagination {
+  list-style: none;
+}
+
+.pagination li {
+  float: left;
+  padding-right: 15px;
+}
+</style>
